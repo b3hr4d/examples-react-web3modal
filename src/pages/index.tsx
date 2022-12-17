27b941c90c53e6d55e63d5d5fe22d4d16aebe7ce
b3hr4d <behradmusiclove@yahoo.com>
2022-12-17 19:28:37 +0300

    test storage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { useWeb3Modal, Web3Button, Web3Modal } from "@web3modal/react"
 import { utils } from "ethers"
+import { useEffect } from "react"
 import { useAccount } from "wagmi"
 import useEthereum from "../hooks/useEthereum"
 import { ethereumClient } from "./_app"
@@ -25,6 +26,42 @@ export default function CustomPage() {
 
     await tx.wait(1)
   }
+
+  useEffect(() => {
+    // Load the todos on mount
+    const todosString = localStorage.getItem("todos")
+    if (todosString) {
+      const todos = JSON.parse("WALLETCONNECT_DEEPLINK_CHOICE")
+      console.log(todos)
+    }
+    // Respond to the `storage` event
+    function storageEventHandler(event: StorageEvent) {
+      console.log(event)
+      const deepLink = window.localStorage.getItem(
+        "WALLETCONNECT_DEEPLINK_CHOICE"
+      )
+      if (deepLink) {
+        try {
+          const _deepLink: { name: string; href: string } = JSON.parse(deepLink)
+          if (_deepLink.href === "https://link.trustwallet.com/wc") {
+            window.localStorage.setItem(
+              "WALLETCONNECT_DEEPLINK_CHOICE",
+              JSON.stringify({ name: "Trust Wallet", href: "trust://" })
+            )
+          }
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (err: any) {
+          console.log("TrustWallet force redirect err", err)
+        }
+      }
+    }
+    // Hook up the event handler
+    window.addEventListener("storage", storageEventHandler)
+    return () => {
+      // Remove the handler when the component unmounts
+      window.removeEventListener("storage", storageEventHandler)
+    }
+  }, [])
 
   return (
     <>
