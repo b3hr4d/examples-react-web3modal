f543275fdeb9e7c53105c0058a6920456af68ff8
b3hr4d <behradmusiclove@yahoo.com>
2022-12-17 19:18:08 +0300

    strage changer

--- src/hooks/useEthereum.ts
+++ src/hooks/useEthereum.ts
@@ -3,6 +3,24 @@ import { useCallback, useEffect, useState } from "react"
 import { ethereumClient } from "../pages/_app"
 import HiVPN from "./contracts"
 
+const handleWalletConnectDeepLink = () => {
+  const deepLink = window.localStorage.getItem("WALLETCONNECT_DEEPLINK_CHOICE")
+  if (deepLink) {
+    try {
+      const _deepLink: { name: string; href: string } = JSON.parse(deepLink)
+      if (_deepLink.href === "https://link.trustwallet.com/wc") {
+        window.localStorage.setItem(
+          "WALLETCONNECT_DEEPLINK_CHOICE",
+          JSON.stringify({ name: "Trust Wallet", href: "trust://" })
+        )
+      }
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      console.log("TrustWallet force redirect err", err)
+    }
+  }
+}
+
 const useEthereum = () => {
   const [{ isConnected, address, connector }, setAccount] = useState<any>({
     isConnected: false,
@@ -19,6 +37,7 @@ const useEthereum = () => {
 
   useEffect(() => {
     ethereumClient.watchAccount((account) => {
+      handleWalletConnectDeepLink()
       setLoading(true)
       console.log("account changed", account.address)
       account.connector?.getSigner().then((signer: any) => {
