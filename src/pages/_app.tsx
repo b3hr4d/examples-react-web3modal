a562877812c34217faf8d58011d4b5af813817de
b3hr4d <behradmusiclove@yahoo.com>
2022-12-17 18:52:19 +0300

    add another connection for trust wallet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,39 @@
-import { EthereumClient, modalConnectors, walletConnectProvider } from '@web3modal/ethereum'
-import { Web3Modal } from '@web3modal/react'
-import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import { configureChains, createClient, WagmiConfig } from 'wagmi'
-import { arbitrum, avalanche, bsc, fantom, mainnet, optimism, polygon } from 'wagmi/chains'
-import '../styles.css'
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum"
+import { Web3Modal } from "@web3modal/react"
+import type { AppProps } from "next/app"
+import { useEffect, useState } from "react"
+import { configureChains, createClient, WagmiConfig } from "wagmi"
+import {
+  arbitrum,
+  avalanche,
+  bsc,
+  fantom,
+  mainnet,
+  optimism,
+  polygon,
+} from "wagmi/chains"
+import "../styles.css"
 
 // 1. Get projectID at https://cloud.walletconnect.com
 if (!process.env.NEXT_PUBLIC_PROJECT_ID) {
-  throw new Error('You need to provide NEXT_PUBLIC_PROJECT_ID env variable')
+  throw new Error("You need to provide NEXT_PUBLIC_PROJECT_ID env variable")
 }
 
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
 
 // 2. Configure wagmi client
 const chains = [mainnet, polygon, optimism, arbitrum, avalanche, fantom, bsc]
-const { provider } = configureChains(chains, [walletConnectProvider({ projectId })])
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId }),
+])
 const wagmiClient = createClient({
   autoConnect: true,
-  connectors: modalConnectors({ appName: 'web3Modal', chains }),
-  provider
+  connectors: modalConnectors({ appName: "web3Modal", chains }),
+  provider,
 })
 
 // 3. Configure modal ethereum client
@@ -28,7 +41,6 @@ export const ethereumClient = new EthereumClient(wagmiClient, chains)
 
 // 4. Wrap your app with WagmiProvider and add <Web3Modal /> compoennt
 export default function App({ Component, pageProps }: AppProps) {
-  const { pathname } = useRouter()
   const [ready, setReady] = useState(false)
 
   useEffect(() => {
@@ -42,11 +54,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </WagmiConfig>
       ) : null}
-
-      {/* Demo purposes only, if custom path is set, we initialize different Web3Modal instance */}
-      {pathname === '/custom' ? null : (
-        <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
-      )}
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
     </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
-import { useWeb3Modal, Web3Button } from "@web3modal/react"
+import { useWeb3Modal, Web3Button, Web3Modal } from "@web3modal/react"
 import { utils } from "ethers"
 import { useAccount } from "wagmi"
-import ThemeControls from "../components/ThemeControls"
 import useEthereum from "../hooks/useEthereum"
+import { ethereumClient } from "./_app"
 
-export default function HomePage() {
+export default function CustomPage() {
   const { isConnected } = useAccount()
   const { open } = useWeb3Modal()
+
   const { getSignature, signer } = useEthereum()
 
   const sendTransaction = async () => {
@@ -27,22 +28,116 @@ export default function HomePage() {
 
   return (
     <>
-      <div className="container">
-        {/* Use predefined button */}
-        <Web3Button />
+      {/* Use predefined button */}
+      <Web3Button />
 
-        {/* Alternatively Use custom button */}
-        {!isConnected ? (
-          <button onClick={() => open()}>Custom</button>
-        ) : (
-          <>
-            <button onClick={() => getSignature()}>Get Signature</button>
-            <button onClick={sendTransaction}>Send Transaction</button>
-          </>
-        )}
-      </div>
+      {/* Use custom button */}
+      {!isConnected ? (
+        <>
+          <button onClick={() => open()}>Or Use Custom Button</button>
+        </>
+      ) : (
+        <>
+          <button onClick={() => getSignature()}>Get Signature</button>
+          <button onClick={sendTransaction}>Send Transaction</button>
+        </>
+      )}
 
-      <ThemeControls />
+      <Web3Modal
+        ethereumClient={ethereumClient}
+        projectId={process.env.NEXT_PUBLIC_PROJECT_ID}
+        mobileWallets={[
+          {
+            id: "trust",
+            name: "Trust Wallet",
+            links: {
+              native: "trust://",
+              universal: "trust://",
+            },
+          },
+          {
+            id: "trust2",
+            name: "Trust Wallet 2",
+            links: {
+              native: "trust://",
+              universal: "https://link.trustwallet.com/wc",
+            },
+          },
+          {
+            id: "trust3",
+            name: "Trust Wallet 3",
+            links: {
+              native: "trust://",
+              universal: "https://link.trustwallet.com/wc?uri=wc",
+            },
+          },
+          {
+            id: "trust4",
+            name: "Trust Wallet 4",
+            links: {
+              native: "https://link.trustwallet.com/wc",
+              universal: "https://link.trustwallet.com/wc",
+            },
+          },
+          {
+            id: "metamask",
+            name: "MetaMask",
+            links: {
+              native: "metamask://",
+              universal: "https://metamask.app.link/wc",
+            },
+          },
+        ]}
+        // Custom Linking Desktop Wallets
+        desktopWallets={[
+          {
+            id: "ledger",
+            name: "Ledger",
+            links: {
+              native: "ledgerlive://",
+              universal: "https://www.ledger.com",
+            },
+          },
+          {
+            id: "zerion",
+            name: "Zerion",
+            links: {
+              native: "zerion://",
+              universal: "https://wallet.zerion.io",
+            },
+          },
+          {
+            id: "tokenary",
+            name: "Tokenary",
+            links: { native: "tokenary://", universal: "https://tokenary.io" },
+          },
+          {
+            id: "oreid",
+            name: "OREID",
+            links: {
+              native: "",
+              universal: "https://www.oreid.io/",
+            },
+          },
+        ]}
+        // Custom Wallet Images
+        walletImages={{
+          metaMask: "/images/wallet_metamask.webp",
+          brave: "/images/wallet_brave.webp",
+          ledger: "/images/wallet_ledger.webp",
+          coinbaseWallet: "/images/wallet_coinbase.webp",
+          zerion: "/images/wallet_zerion.webp",
+          trust: "/images/wallet_trust.webp",
+          rainbow: "/images/wallet_rainbow.webp",
+          oreid: "/images/wallet_oreid.svg",
+        }}
+        // Custom Chain Images
+        chainImages={{
+          137: "/images/chain_polygon.webp",
+          10: "/images/chain_optimism.webp",
+          42161: "/images/chain_arbitrum.webp",
+        }}
+      />
     </>
   )
 }
